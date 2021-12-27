fd07c447611ca73462ced0247c111f3c76468baf
Buzeqq <miloszchoj@gmail.com>
2021-12-27 18:56:52 +0100

    Added image reading header file

--- edge_detection/edge_detection/main.cpp
+++ edge_detection/edge_detection/main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
-using std::cout;
+#include <stdint.h>
 
-int main()
-{
-	cout << "hello world";
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int main() {
+  int width, height, bpp;
+
+  uint8_t* rgb_image = stbi_load("image.png", &width, &height, &bpp, 3);
+
+  stbi_image_free(rgb_image);
+
+  return 0;
 }
