bdd412c1d5ce4e3b7eca0bf7067700dd7dd31628
qaziok <72215548+qaziok@users.noreply.github.com>
2021-12-28 18:43:00 +0100

    lekko poprawiony kod

--- edge_detection/edge_detection/main.cpp
+++ edge_detection/edge_detection/main.cpp
@@ -17,10 +17,6 @@
 #define WEAK 50
 #define STRONG 255
 
-const int8_t laplacian[] = { -1, -1, -1,
-                             -1,  8, -1,
-                             -1, -1, -1 };
-
 const int8_t sobel_x[] = { 1, 0, -1,
                            2, 0, -2,
                            1, 0, -1 };
@@ -36,24 +32,8 @@ const float gaussian[] = { 0.0030,0.0133,0.0219,0.0133,0.0030,
     0.0133,    0.0596,    0.0983,    0.0596,    0.0133,
     0.0030,    0.0133,    0.0219,    0.0133,    0.0030 };
 
-
-
-uint8_t pixelInPosition(int x, int y, std::vector<uint8_t>& vector, int width, int height) {
-  if (x > width - 1)  x -= 2;
-  else if (x < 0) x += 2;
-
-  if (y > height - 1) y -= 2;
-  else if (y < 0) y += 2;
-
-  int index = y * (long long)width + x;
-  if (index < 0 || index >= vector.size()) {
-      
-    return -1;
-  }
-  return vector[y * (long long)width + x];
-}
-
-float angleInPosition(int x, int y, std::vector<float>& vector, int width, int height) {
+template <typename T>
+T pixelInPosition(int x, int y, std::vector<T>& vector, int width, int height) {
     if (x > width - 1)  x -= 2;
     else if (x < 0) x += 2;
 
@@ -68,7 +48,9 @@ float angleInPosition(int x, int y, std::vector<float>& vector, int width, int h
     return vector[y * (long long)width + x];
 }
 
-void filter(const int x, const int y, std::vector<uint8_t>& inputImage, std::vector<uint8_t>& outputImage, int width, int height) {
+void filter(const int x, const int y, std::vector<uint8_t>& inputImage,
+    std::vector<uint8_t>& outputImage, int width, int height) {
+
   int16_t newPixelValue = 0;
   int matrix_size = sqrt(sizeof(gaussian) / sizeof(gaussian[0]));
   int iterator = 0;
@@ -77,7 +59,8 @@ void filter(const int x, const int y, std::vector<uint8_t>& inputImage, std::vec
   {
       for (int j = -matrix_size / 2; j <= matrix_size / 2; j++)
       {
-          newPixelValue += (int16_t)((float)pixelInPosition(x + i, y + j, inputImage, width, height) * gaussian[iterator]);
+          newPixelValue += (int16_t)((float)pixelInPosition(x + i, y + j,
+              inputImage, width, height) * gaussian[iterator]);
           iterator++;
       }
   }
@@ -89,6 +72,7 @@ void filter(const int x, const int y, std::vector<uint8_t>& inputImage, std::vec
 
 void filter_sobel(const int x, const int y, std::vector<uint8_t>& inputImage, 
     std::vector<uint8_t>& outputImage, std::vector<float>& outputAngle, int width, int height) {
+
     int16_t newPixelValue = 0;
     int16_t newPixelValue_x = 0;
     int16_t newPixelValue_y = 0;
@@ -114,8 +98,9 @@ void filter_sobel(const int x, const int y, std::vector<uint8_t>& inputImage,
 
 void NonMaximumSuppression(const int x, const int y, std::vector<uint8_t>& inputImage, 
     std::vector<float>& inputAngle, std::vector<uint8_t>& outputImage, int width, int height) {
+
     uint8_t first_neighbour=0, second_neighbour=0;
-    double angle = 180 * (double)angleInPosition(x, y, inputAngle, width, height) / M_PI;
+    double angle = 180 * (double)pixelInPosition(x, y, inputAngle, width, height) / M_PI;
     if ((0 <= angle < 22.5) or (157.5 <= angle <= 180)) {
         first_neighbour = pixelInPosition(x, y + 1, inputImage, width, height);
         second_neighbour = pixelInPosition(x, y - 1, inputImage, width, height);
@@ -143,6 +128,7 @@ void NonMaximumSuppression(const int x, const int y, std::vector<uint8_t>& input
 
 void DoubleThreshold(const int x, const int y, std::vector<uint8_t>& inputImage,
     std::vector<uint8_t>& outputImage, int width, int height) {
+
     int pixel = pixelInPosition(x, y, inputImage, width, height);
     if (pixel >= 75)
         outputImage.push_back(STRONG);
@@ -154,6 +140,7 @@ void DoubleThreshold(const int x, const int y, std::vector<uint8_t>& inputImage,
 
 void Hysteresis(const int x, const int y, std::vector<uint8_t>& inputImage,
     std::vector<uint8_t>& outputImage, int width, int height) {
+
     int pixel = pixelInPosition(x, y, inputImage, width, height);
     bool strong = false;
     if (pixel == WEAK) {
@@ -177,81 +164,92 @@ void Hysteresis(const int x, const int y, std::vector<uint8_t>& inputImage,
         outputImage.push_back(pixel);
 }
 
+void edge_detection(std::vector<uint8_t>& inputImage, std::vector<uint8_t>& outputImage,
+    int width, int height) {
+
+    std::vector<uint8_t> gaussianImage;
+    gaussianImage.reserve(inputImage.size());
+    std::vector<uint8_t> sobelImage;
+    sobelImage.reserve(inputImage.size());
+    std::vector<float> sobelAngle;
+    sobelAngle.reserve(inputImage.size());
+    std::vector<uint8_t> suppressionImage;
+    suppressionImage.reserve(inputImage.size());
+    std::vector<uint8_t> thresholdImage;
+    thresholdImage.reserve(inputImage.size());
+    std::vector<uint8_t> hysteresisImage;
+    hysteresisImage.reserve(inputImage.size());
+
+    int x = 0;
+    int y = 0;
+    for (auto pixel : inputImage) {
+        filter(x, y, inputImage, gaussianImage, width, height);
+        x++;
+        if (x == width) {
+            x = x % width;
+            y++;
+        }
+    }
+
+    x = 0;
+    y = 0;
+    for (auto pixel : gaussianImage) {
+        filter_sobel(x, y, gaussianImage, sobelImage, sobelAngle, width, height);
+        x++;
+        if (x == width) {
+            x = x % width;
+            y++;
+        }
+    }
+
+    x = 0;
+    y = 0;
+    for (auto pixel : sobelImage) {
+        NonMaximumSuppression(x, y, sobelImage, sobelAngle, suppressionImage, width, height);
+        x++;
+        if (x == width) {
+            x = x % width;
+            y++;
+        }
+    }
+
+    x = 0;
+    y = 0;
+    for (auto pixel : suppressionImage) {
+        DoubleThreshold(x, y, suppressionImage, thresholdImage, width, height);
+        x++;
+        if (x == width) {
+            x = x % width;
+            y++;
+        }
+    }
+
+    x = 0;
+    y = 0;
+    for (auto pixel : thresholdImage) {
+        Hysteresis(x, y, thresholdImage, outputImage, width, height);
+        x++;
+        if (x == width) {
+            x = x % width;
+            y++;
+        }
+    }
+}
+
+
 int main() {
   int width, height, bpp;
 
-  uint8_t* grayScaleImage = stbi_load("emma.jpg", &width, &height, &bpp, 1);
+  uint8_t* grayScaleImage = stbi_load("kenji.jpg", &width, &height, &bpp, 1);
 
   std::vector<uint8_t> inputImage(grayScaleImage, grayScaleImage + (width * (long long)height));
-  std::vector<uint8_t> gaussianImage;
-  gaussianImage.reserve(inputImage.size());
-  std::vector<uint8_t> sobelImage;
-  sobelImage.reserve(inputImage.size());
-  std::vector<float> sobelAngle;
-  sobelAngle.reserve(inputImage.size());
-  std::vector<uint8_t> suppressionImage;
-  suppressionImage.reserve(inputImage.size());
-  std::vector<uint8_t> thresholdImage;
-  thresholdImage.reserve(inputImage.size());
-  std::vector<uint8_t> hysteresisImage;
-  hysteresisImage.reserve(inputImage.size());
+  std::vector<uint8_t> outputImage;
+  outputImage.reserve(inputImage.size());
 
-  int x = 0;
-  int y = 0;
-  for (auto pixel : inputImage) {
-    filter(x, y, inputImage, gaussianImage, width, height);
-    x++;
-    if (x == width) {
-      x = x % width;
-      y++;
-    }
-  }
+  edge_detection(inputImage, outputImage, width, height);
 
-  x = 0;
-  y = 0;
-  for (auto pixel : gaussianImage) {
-      filter_sobel(x, y, gaussianImage, sobelImage, sobelAngle, width, height);
-      x++;
-      if (x == width) {
-          x = x % width;
-          y++;
-      }
-  }
-
-  x = 0;
-  y = 0;
-  for (auto pixel : sobelImage) {
-      NonMaximumSuppression(x, y, sobelImage, sobelAngle, suppressionImage,  width, height);
-      x++;
-      if (x == width) {
-          x = x % width;
-          y++;
-      }
-  }
-
-  x = 0;
-  y = 0;
-  for (auto pixel : suppressionImage) {
-      DoubleThreshold(x, y, suppressionImage, thresholdImage, width, height);
-      x++;
-      if (x == width) {
-          x = x % width;
-          y++;
-      }
-  }
-
-  x = 0;
-  y = 0;
-  for (auto pixel : thresholdImage) {
-      Hysteresis(x, y, thresholdImage, hysteresisImage, width, height);
-      x++;
-      if (x == width) {
-          x = x % width;
-          y++;
-      }
-  }
-
-  stbi_write_png("emma_output.png", width, height, CHANNEL_NUM, hysteresisImage.data(), width * CHANNEL_NUM);
+  stbi_write_png("kenji_output.png", width, height, CHANNEL_NUM,
+      outputImage.data(), width * CHANNEL_NUM);
   stbi_image_free(grayScaleImage);
 
   return 0;
